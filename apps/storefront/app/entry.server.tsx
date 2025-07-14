70036c76beeecf76360adb0f86f1c7ff60b4e3ef
Scott Lingner <56003456+slingner@users.noreply.github.com>
2025-07-13 20:38:33 -0700

    update

--- apps/storefront/app/entry.server.tsx
+++ apps/storefront/app/entry.server.tsx
@@ -1,9 +1,9 @@
-import { PassThrough } from 'stream';
-import { createReadableStreamFromReadable } from '@react-router/node';
-import * as Sentry from '@sentry/remix';
-import { isbot } from 'isbot';
-import { renderToPipeableStream } from 'react-dom/server';
-import { EntryContext, ServerRouter } from 'react-router';
+import { PassThrough } from "stream";
+import { createReadableStreamFromReadable } from "@react-router/node";
+import * as Sentry from "@sentry/remix";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+import { EntryContext, ServerRouter } from "react-router";
 
 if (process.env.SENTRY_DSN)
   Sentry.init({
@@ -17,46 +17,56 @@ export const handleError = Sentry.sentryHandleError;
 const ABORT_DELAY = 5000;
 
 export default function handleRequest(
-  ...props: [request: Request, responseStatusCode: number, responseHeaders: Headers, reactRouterContext: EntryContext]
+  ...props: [
+    request: Request,
+    responseStatusCode: number,
+    responseHeaders: Headers,
+    reactRouterContext: EntryContext
+  ]
 ) {
-  return isbot(props[0].headers.get('user-agent')) ? handleBotRequest(...props) : handleBrowserRequest(...props);
+  return isbot(props[0].headers.get("user-agent"))
+    ? handleBotRequest(...props)
+    : handleBrowserRequest(...props);
 }
 
 function handleBotRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  reactRouterContext: EntryContext,
+  reactRouterContext: EntryContext
 ) {
   return new Promise((resolve, reject) => {
     let didError = false;
 
-    const { pipe, abort } = renderToPipeableStream(<ServerRouter context={reactRouterContext} url={request.url} />, {
-      onAllReady() {
-        const body = new PassThrough();
+    const { pipe, abort } = renderToPipeableStream(
+      <ServerRouter context={reactRouterContext} url={request.url} />,
+      {
+        onAllReady() {
+          const body = new PassThrough();
 
-        const stream = createReadableStreamFromReadable(body);
+          const stream = createReadableStreamFromReadable(body);
 
-        responseHeaders.set('Content-Type', 'text/html');
+          responseHeaders.set("Content-Type", "text/html");
 
-        resolve(
-          new Response(stream, {
-            headers: responseHeaders,
-            status: didError ? 500 : responseStatusCode,
-          }),
-        );
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          );
 
-        pipe(body);
-      },
-      onShellError(error: unknown) {
-        reject(error);
-      },
-      onError(error: unknown) {
-        didError = true;
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          didError = true;
 
-        console.error(error);
-      },
-    });
+          console.error(error);
+        },
+      }
+    );
 
     setTimeout(abort, ABORT_DELAY);
   });
@@ -66,36 +76,39 @@ function handleBrowserRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  reactRouterContext: EntryContext,
+  reactRouterContext: EntryContext
 ) {
   return new Promise((resolve, reject) => {
     let didError = false;
 
-    const { pipe, abort } = renderToPipeableStream(<ServerRouter context={reactRouterContext} url={request.url} />, {
-      onShellReady() {
-        const body = new PassThrough();
+    const { pipe, abort } = renderToPipeableStream(
+      <ServerRouter context={reactRouterContext} url={request.url} />,
+      {
+        onShellReady() {
+          const body = new PassThrough();
 
-        const stream = createReadableStreamFromReadable(body);
-        responseHeaders.set('Content-Type', 'text/html');
+          const stream = createReadableStreamFromReadable(body);
+          responseHeaders.set("Content-Type", "text/html");
 
-        resolve(
-          new Response(stream, {
-            headers: responseHeaders,
-            status: didError ? 500 : responseStatusCode,
-          }),
-        );
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          );
 
-        pipe(body);
-      },
-      onShellError(err: unknown) {
-        reject(err);
-      },
-      onError(error: unknown) {
-        didError = true;
+          pipe(body);
+        },
+        onShellError(err: unknown) {
+          reject(err);
+        },
+        onError(error: unknown) {
+          didError = true;
 
-        console.error(error);
-      },
-    });
+          console.error(error);
+        },
+      }
+    );
 
     setTimeout(abort, ABORT_DELAY);
   });
